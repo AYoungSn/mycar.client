ad92495bea31c2975c27d5a55941411a25641166
AYoungSn <dudtjs0920@naver.com>
2023-07-04 16:43:48 +0900

    [feat] #3 내장 색상 리스트

--- src/components/color/Color.jsx
+++ src/components/color/Color.jsx
@@ -1,19 +1,13 @@
-import { useState } from "react";
 import { OptionHead } from "../styled/Option";
 import { Exterior } from "./Exterior";
-import { useRecoilState } from "recoil";
-import { exteriorState, interiorState } from "../../utils/recoil/color";
+import { Interior } from "./Interior";
 
 export function Color(props) {
-	const [curExterior, setCurExterior] = useRecoilState(exteriorState);
-	const [curInterior, setCurInterior] = useRecoilState(interiorState);
 	return (
 		<div>
 			<OptionHead>색상</OptionHead>
-			<Exterior data={props.exterior} cur={curExterior}
-				onChange={(id) => {
-					setCurExterior(id);
-				}}/>
+			<Exterior data={props.exterior}/>
+			<Interior data={props.interior}/>
 		</div>
 	)
 }
---
+++ src/components/color/Interior.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components"
+import { FlexUl } from "../styled/Flex"
+import { ColorBtn, OptionColor, OptionName, OptionTitle } from "../styled/Option"
+import { useRecoilState } from "recoil";
+import { interiorState } from "../../utils/recoil/color";
+import { useEffect, useState } from "react";
+
+const InteriorItem = styled.li`
+	margin-bottom: 25px;
+`;
+
+export function Interior(props) {
+	const [interiorId, setInteriorId] = useRecoilState(interiorState);
+	const [curInterior, setCurInterior] = useState('');
+	useEffect(() => {
+		async function initInterior() {
+			if (interiorId === undefined || interiorId === 0) {
+				setInteriorId(props.data[0]?.id);
+				setCurInterior(props.data[0]?.name);
+			}
+		}
+		initInterior();
+	}, [props.data.length]);
+	return (
+		<section>
+			<OptionTitle>
+				<OptionName>내장색상</OptionName>
+				<OptionColor>{curInterior}</OptionColor>
+			</OptionTitle>
+			<FlexUl>
+				{
+					props.data?.map((interior, id) => {
+						return (
+							<InteriorItem key={interior.id}>
+								<ColorBtn width={"496px"} height={"75px"} style={{backgroundImage:`url(${interior.imgUri})`}}
+									onClick={() => {
+										setInteriorId(interior.id);
+										setCurInterior(interior.name);
+									}}/>
+							</InteriorItem>
+						)
+					})
+				}
+			</FlexUl>
+		</section>
+	)
+}
